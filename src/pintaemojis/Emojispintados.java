fc10af04d6333623a964080e3bd3a018915c1bb4
sergius39 <122611800+sergius39@users.noreply.github.com>
2023-02-11 16:49:00 +0100

    Update Emojispintados.java

--- src/pintaemojis/Emojispintados.java
+++ src/pintaemojis/Emojispintados.java
@@ -8,6 +8,12 @@ public class Emojispintados {
 			System.out.printf("\uD83D\uDE00");
 			}
 
+		for (int i=0; i<10; i++) {
+		System.out.printf("\uD83D\uDE00");
+		System.out.println("\uD83D\uDE02");
+			}
+		
+			
 	}
 
 }
